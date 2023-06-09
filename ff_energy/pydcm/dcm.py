1dc8fd2bface696cfea704bb2a75634491610a30
EricBoittier <eric.boittier@icloud.com>
2023-06-09 11:55:05 +0200

    working on dcm code from pcnccr

--- ff_energy/pydcm/dcm.py
+++ ff_energy/pydcm/dcm.py
@@ -1,15 +1,234 @@
+from ff_energy.pydcm.dcm_fortran import dcm_fortran
+import numpy as np
 # Basics
+import os
+import pickle
+import pandas as pd
+import argparse
+# Optimization
+from scipy.optimize import minimize
 
-# Utilities
+
+mdcm = dcm_fortran
+
+espform = "/home/boittier/Documents/phd/ff_energy/cubes/dcm/nms/" \
+          "test_nms_0_0.xyz_esp.cube"
+densform = "/home/boittier/Documents/phd/ff_energy/cubes/dcm/nms/" \
+           "test_nms_0_0.xyz_dens.cube"
+
+scan_fesp = [espform]
+scan_fdns = [densform]
+
+mdcm_cxyz = "/home/boittier/Documents/phd/ff_energy/ff_energy/pydcm/sources/" \
+    "dcm8.xyz"
+mdcm_clcl = "/home/boittier/Documents/phd/ff_energy/ff_energy/pydcm/sources/" \
+            "dcm.mdcm"
+
+local_pos = None
 
 
-# Miscellaneous
+def mdcm_set_up(scan_fesp, scan_fdns,
+                mdcm_cxyz=None,
+                mdcm_clcl=None,
+                local_pos=None):
+    mdcm.dealloc_all()
+    Nfiles = len(scan_fesp)
+    Nchars = int(np.max([
+        len(filename) for filelist in [scan_fesp, scan_fdns]
+        for filename in filelist]))
+    esplist = np.empty([Nfiles, Nchars], dtype='c')
+    dnslist = np.empty([Nfiles, Nchars], dtype='c')
+    for ifle in range(Nfiles):
+        esplist[ifle] = "{0:{1}s}".format(scan_fesp[ifle], Nchars)
+        dnslist[ifle] = "{0:{1}s}".format(scan_fdns[ifle], Nchars)
+    # Load cube files, read MDCM global and local files
+    mdcm.load_cube_files(Nfiles, Nchars, esplist.T, dnslist.T)
+    if mdcm_clcl is not None:
+        mdcm.load_clcl_file(mdcm_clcl)
+    if mdcm_cxyz is not None:
+        mdcm.load_cxyz_file(mdcm_cxyz)
+    if local_pos is not None:
+        mdcm.set_clcl(local_pos)
+    # Get and set global MDCM array (to check if manipulation is possible)
+    cxyz = mdcm.mdcm_cxyz
+    mdcm.set_cxyz(cxyz)
+    # Write MDCM global from local and Fitted ESP cube files
+    mdcm.write_cxyz_files()
+    mdcm.write_mdcm_cube_files()
+    return mdcm
 
-class DCM:
+bohr_to_a = 0.529177
 
-    def __init__(self):
-        """
-        Initialize
-        """
 
-        pass
+def get_clcl(local_pos, charges):
+    NCHARGES = len(charges)
+    _clcl_ = np.zeros(NCHARGES)
+    for i in range(NCHARGES):
+        if (i + 1) % 4 == 0:
+            _clcl_[i] = charges[i]
+        else:
+            _clcl_[i] = local_pos[i - ((i) // 4)]
+    return _clcl_
+
+
+def set_bounds(local_pos, change=0.1):
+    bounds = []
+    for i, x in enumerate(local_pos):
+        bounds.append((x - abs(x) * change, x + abs(x) * change))
+    return tuple(bounds)
+
+
+def mdcm_set_up(scan_fesp, scan_fdns,
+                mdcm_cxyz=None,
+                mdcm_clcl=None,
+                local_pos=None):
+    mdcm.dealloc_all()
+
+    Nfiles = len(scan_fesp)
+    Nchars = int(np.max([
+        len(filename) for filelist in [scan_fesp, scan_fdns]
+        for filename in filelist]))
+
+    esplist = np.empty([Nfiles, Nchars], dtype='c')
+    dnslist = np.empty([Nfiles, Nchars], dtype='c')
+
+    for ifle in range(Nfiles):
+        esplist[ifle] = "{0:{1}s}".format(scan_fesp[ifle], Nchars)
+        dnslist[ifle] = "{0:{1}s}".format(scan_fdns[ifle], Nchars)
+
+    # Load cube files, read MDCM global and local files
+    mdcm.load_cube_files(Nfiles, Nchars, esplist.T, dnslist.T)
+    mdcm.load_clcl_file(mdcm_clcl)
+    mdcm.load_cxyz_file(mdcm_cxyz)
+
+    # Get and set local MDCM array (to check if manipulation is possible)
+    clcl = mdcm.mdcm_clcl
+    mdcm.set_clcl(clcl)
+
+    if local_pos is not None:
+        mdcm.set_clcl(local_pos)
+
+    # Get and set global MDCM array (to check if manipulation is possible)
+    cxyz = mdcm.mdcm_cxyz
+    mdcm.set_cxyz(cxyz)
+
+    # Write MDCM global from local and Fitted ESP cube files
+    mdcm.write_cxyz_files()
+    mdcm.write_mdcm_cube_files()
+
+    return mdcm
+
+
+def optimize_mdcm(mdcm, clcl, outdir, outname, l2=100.0):
+    # Get RMSE, averaged or weighted over ESP files, or per ESP file each
+    rmse = mdcm.get_rmse()
+    print(rmse)
+
+    #  save an array containing original charges
+    charges = clcl.copy()
+    local_pos = clcl[np.mod(np.arange(clcl.size) + 1, 4) != 0]
+    local_ref = local_pos.copy()
+
+    def mdcm_rmse(local_pos, local_ref=local_ref, l2=l2):
+        """Minimization routine"""
+        _clcl_ = get_clcl(local_pos, charges)
+        mdcm.set_clcl(_clcl_)
+        rmse = mdcm.get_rmse()
+        if local_ref is not None:
+            l2diff = l2 * np.sum((local_pos - local_ref) ** 2) / local_pos.shape[0]
+            # print(rmse, l2diff)
+            rmse += l2diff
+        return rmse
+
+    # Apply simple minimization without any feasibility check (!)
+    # Leads to high amplitudes of MDCM charges and local positions
+    res = minimize(
+        mdcm_rmse, local_pos,
+        method="L-BFGS-B",
+        options={'disp': None, 'maxls': 20, 'iprint': -1, 'gtol': 1e-06,
+                 'eps': 1e-09, 'maxiter': 15000,
+                 'ftol': 1e-8, 'maxcor': 10, 'maxfun': 15000})
+    print(res)
+    # Recompute final RMSE each
+    rmse = mdcm.get_rmse()
+    print(rmse)
+    mdcm.write_cxyz_files()
+    #  get the local charges array after optimization
+    clcl_out = get_clcl(res.x, charges)
+    difference = np.sum((res.x - local_ref) ** 2) / local_pos.shape[0]
+    print("charge RMSD:", difference)
+
+    obj_name = os.path.join(outdir, f"pickles/{outname}_clcl.obj")
+    #  save as pickle
+    filehandler = open(obj_name, "wb")
+    pickle.dump(clcl_out, filehandler)
+    # Not necessary but who knows when it becomes important to deallocate all
+    # global arrays
+    mdcm.dealloc_all()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Scan and average for fMDCM')
+    parser.add_argument('-n', '--nodes_to_avg', help='', required=True,
+                        type=int)
+    parser.add_argument('-l', '--local_pos', help='', default=None, type=str)
+    parser.add_argument('-l2', '--l2', help='lambda coef. for l2 reg.',
+                        default=100.0, type=float)
+    parser.add_argument('-o', '--outdir', help='', default=None, type=str)
+    parser.add_argument('-opt', '--opt', help='', default=False, type=bool)
+    parser.add_argument('-esp', '--esp', help='format string for esp files',
+                        default=None, type=str)
+    parser.add_argument('-dens', '--dens', help='format string for density files',
+                        default=None, type=str)
+
+    args = parser.parse_args()
+    print(' '.join(f'{k}={v}\n' for k, v in vars(args).items()))
+
+    if args.local_pos is not None:
+        local = pd.read_pickle(args.local_pos)
+    else:
+        print("WARNING: No local positions specified")
+        local = None
+
+    if args.mdcm_clcl is not None:
+        mdcm_clcl = args.mdcm_clcl
+    else:
+        print("WARNING: No MDCM clcl file specified")
+    if args.mdcm_xyz is not None:
+        mdcm_xyz = args.mdcm_xyz
+    else:
+        print("WARNING: No MDCM xyz file specified")
+    if args.esp is not None:
+        esp = args.esp
+    else:
+        raise ValueError("No ESP file specified")
+
+    if args.dens is not None:
+        dens = args.dens
+    else:
+        raise ValueError("No density file specified")
+
+    if args.outdir is not None:
+        outdir = args.outdir
+    else:
+        print("WARNING: No output directory specified")
+
+    if args.nodes_to_avg is not None:
+        i = args.nodes_to_avg
+    else:
+        print("WARNING: No nodes to average specified")
+
+    mdcm = mdcm_set_up([esp.format(i)], [dens.format(i)],
+                       mdcm_cxyz=mdcm_xyz,
+                       mdcm_clcl=mdcm_clcl,
+                       local_pos=local)
+
+    clcl = mdcm.mdcm_clcl
+
+    if args.opt:
+        print("Optimizing")
+        outname = esp.format(i).split("/")[-1]
+        optimize_mdcm(mdcm, clcl, args.outdir, outname, l2=args.l2)
+    else:
+        rmse = mdcm.get_rmse()
+        print("RMSE:", i, rmse)
--- ff_energy/pydcm/tests/dcm.py
+++ ff_energy/pydcm/tests/dcm.py
@@ -1,9 +1,18 @@
 import unittest
+from ff_energy.pydcm.dcm import mdcm, mdcm_set_up, scan_fesp, scan_fdns, \
+    mdcm_cxyz, mdcm_clcl, local_pos
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+
+    def test_dcm_fortran(self):
+        m = mdcm_set_up(scan_fesp, scan_fdns,
+                        local_pos=local_pos,
+                        mdcm_cxyz=mdcm_cxyz,
+                        mdcm_clcl=mdcm_clcl)
+        print(m.get_rmse())
 
 
 if __name__ == '__main__':
